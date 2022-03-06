a60281319044a938fe89f5a85044487e5ae2925a
zer0deck <zer0deck@icloud.com>
2022-03-06 15:04:15 +0300

    add facing left/right

--- src/Code/importer.py
+++ src/Code/importer.py
@@ -7,8 +7,8 @@ def import_folder(path):
     for _,_,files in walk(path):
         for file in files:
             if file != '.DS_Store':
-                f_path = f'{path}/{file}'
-                print(f_path)
+                # f_path = f'{path}/{file}'
+                # print(f_path)
                 image = pygame.transform.scale(pygame.image.load(f'{path}/{file}').convert_alpha(), (TILESIZE, TILESIZE))
                 images.append(image)
     return images
--- src/Code/player.py
+++ src/Code/player.py
@@ -13,8 +13,10 @@ class Player(pygame.sprite.Sprite):
         self.frame_index = 0
         # ! don't do animation speed aliquot to 1
         self.animation_speed = 0.3
-        self.image = self.animations['idle'][self.frame_index]
+        self.status = 'idle'
+        self.image = self.animations[self.status][self.frame_index]
         self.rect = self.image.get_rect(topleft = pos)
+        self.facing = True
 
         # movement params
         self.direction = pygame.math.Vector2(0,0)
@@ -24,27 +26,44 @@ class Player(pygame.sprite.Sprite):
 
     def assets(self):
         c_path = '/Users/zer0deck/Documents/Документы/Git/BaldErDash/src/Assets/Sprites/Main_Character/Player/'
-        self.animations = {'idle': [], 'run': [], 'jump': [], 'roll': [], 'climb': [], 'die':[]}
+        self.animations = {'idle': [], 'run': [], 'jump': [], 'roll': [], 'climb': [], 'die':[], 'fall': []}
 
         for animation in self.animations:
             path = c_path + animation
             self.animations[animation] = import_folder(path)
 
-    def animate(self):
-        animation = self.animations['idle']
+    def animate(self, status):
+        animation = self.animations[status]
         self.frame_index += self.animation_speed
 
         if self.frame_index > len(animation):
             self.frame_index = 0
-        self.image = animation[int(self.frame_index)]
+        
+        if self.facing:
+            self.image = animation[int(self.frame_index)]
+        else:
+            self.image = pygame.transform.flip(animation[int(self.frame_index)], True, False)
+
+    def get_status(self):
+        if self.direction.y < 0:
+            self.status = 'jump'
+        elif self.direction.y > 1:
+            self.status = 'fall'
+        else:
+            if self.direction.x != 0:
+                self.status = 'run'
+            else:
+                self.status = 'idle'
 
     def input(self):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_RIGHT] or keys [pygame.K_d]:
             self.direction.x = 1
+            self.facing = True
         elif keys[pygame.K_LEFT] or keys [pygame.K_a]:
             self.direction.x = -1
+            self.facing = False
         else:
             self.direction.x = 0
         
@@ -61,5 +80,6 @@ class Player(pygame.sprite.Sprite):
     def update(self):
         self.input()
         self.rect.x += self.direction.x * self.speed
-        self.animate()
+        self.get_status()
+        self.animate(self.status)
 
