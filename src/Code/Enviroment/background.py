9395988668c2a508e2258f4f4eb63a1929ae692d
zer0deck <zer0deck@icloud.com>
2022-03-20 16:34:08 +0300

    small fix

--- src/Code/Enviroment/background.py
+++ src/Code/Enviroment/background.py
@@ -4,6 +4,7 @@ from Code.settings import WIDTH, HEIGHT
 
 class Background:
     def __init__(self, mapname, screen):
+        mapname = 'Forest'
         self.cloud = pygame.image.load(f'src/Assets/Backgrounds/{mapname}/bg_5.png')
         self.background = pygame.image.load(f'src/Assets/Backgrounds/{mapname}/bg_4.png')
         self.middle = pygame.image.load(f'src/Assets/Backgrounds/{mapname}/bg_3.png')
